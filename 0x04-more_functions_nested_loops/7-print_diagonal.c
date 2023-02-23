7d29ed387abfb620e742adf2978161613b8bb0d1
Malibecky <beckymali330@gmail.com>
2023-02-23 02:19:53 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,34 +1,28 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal line on the terminal
+ * print_diagonal - diagonal lines made of backslashes
  * @n: number of \ to be printed
- *
- * Return: Always 0
+ * Return: void
  */
+
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i, j;
+	int i = 0, ii;
 
-		for (i = 0; i < n; i++)
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < i)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else if (j < i)
-				{
-					_putchar(' ');
-				}
-			}
+			_putchar(' ');
+			ii++;
 		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
 	}
+	if (i == 0)
+		_putchar('\n');
 }
