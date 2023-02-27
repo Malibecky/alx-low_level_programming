58f93454ceab6bd0c35fa407a958227155a5ce8c
Malibecky <beckymali330@gmail.com>
2023-02-27 07:46:54 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int_strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int becky = 0;
+
+	while (*s != '\0')
+	{
+		becky++;
+		s++;
+	}
+	return (becky);
+
+}
